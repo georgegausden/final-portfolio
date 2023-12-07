b4d58619cdd83fc1716c067204fbb42203b8d72a
georgegausden <89808071+georgegausden@users.noreply.github.com>
2023-12-07 15:46:39 -0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,48 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+
+      
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+
+      boxShadow: {
+        'inner-light': 'inset 0 0 2px rgba(0, 0, 0, 0.5)', // black color
+        'inner-dark': 'inset 0 0 2px rgba(255, 255, 255, 0.5)', // orange color
+      },
+
+      animation: {
+        flip: "flip 6s infinite steps(2, end)",
+        rotate: "rotate 3s linear infinite both",
+      },
+      keyframes: {
+        flip: {
+          to: {
+            transform: "rotate(360deg)",
+          },
+        },
+        rotate: {
+          to: {
+            transform: "rotate(90deg)",
+          },
+        },
+      },
+
+      
+
+      
+    },
+  },
+  plugins: [],
+}
+export default config
